809a5516965d3ea41fc444e3e94c76580f6c8144
sshleifer <sam@kensho.com>
2019-04-11 12:26:20 -0700

    remove unused kwargs

--- cv_indices.py
+++ cv_indices.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pickle
 import numpy as np
-
+from tqdm import *
 
 def pickle_save(obj, path):
     with open(path, 'wb') as f:
@@ -31,8 +31,7 @@ def shape_assert(a, b):
     assert a.shape[0] == b.shape[0], message
 
 
-def cross_val_predict_proba_df(m, X, y, test_X, n_splits=5, binary=True, sample_weight=None,
-                               stratified=False):
+def cross_val_predict_proba_df(m, X, y, test_X, binary=True, sample_weight=None, cv_path='tr_indices.pkl'):
     shape_assert(X, y)
     predict_proba_fn = predict_proba_fn_binary if binary else predict_proba
     if binary:
@@ -44,7 +43,7 @@ def cross_val_predict_proba_df(m, X, y, test_X, n_splits=5, binary=True, sample_
     all_indices = X.index
     test_preds = []
 
-    for tr_idx in pickle_load('tr_indices.pkl'):
+    for tr_idx in pickle_load(cv_path):
         test_idx = all_indices.drop(tr_idx)
         if sample_weight is None:
             m.fit(X.iloc[tr_idx, :], y.iloc[tr_idx])
@@ -53,3 +52,39 @@ def cross_val_predict_proba_df(m, X, y, test_X, n_splits=5, binary=True, sample_
         preds.iloc[test_idx] = predict_proba_fn(m, X.iloc[test_idx, :])
         test_preds.append(predict_proba_fn(test_X))
     return preds, test_preds
+
+from fastai.tabular import *
+from fastai.callbacks import *
+def cross_val_predict_fastai(train, cat_names, cont_names, test_list, procs,allowed_features, dep_var='target', ):
+    all_te_probas = []
+
+    all_indices = train.index
+    val_preds = pd.Series({k: np.nan for k in X.index})
+    for i, tr_idx in tqdm_notebook(list(enumerate(pickle_load('tr_indices.pkl')))):
+        val_idx = all_indices.drop(tr_idx)
+        _cat_names = set(cat_names).intersection(allowed_features)
+        _cont_names = set(cat_names).intersection(allowed_features)
+        data = (TabularList.from_df(
+            train, path='train.csv',
+            cat_names=cat_names, cont_names=_cont_names, procs=procs)
+                .split_by_idx(val_idx)
+                .label_from_df(cols=dep_var)
+                .add_test(test_list)
+                .databunch(bs=1024))
+        learn = tabular_learner(data, layers=[200, 100], metrics=accuracy)
+        save_name = f'best_classif_fold_{i}'
+        callbacks = [
+            CSVLogger(learn, filename='csv_logs_for_{i}'),
+            SaveModelCallback(learn, name=save_name),
+            EarlyStoppingCallback(learn, patience=2),
+            GradientClipping(learn, 1.),
+        ]
+        learn.fit_one_cycle(
+            8, .03, callbacks=callbacks
+        )
+        learn = learn.load(save_name)
+        val_probas = (learn.get_preds(ds_type=DatasetType.Valid)[0][:, 1].cpu().numpy())
+        val_preds.iloc[val_idx] = val_probas
+        te_probas = (learn.get_preds(ds_type=DatasetType.Test)[0][:, 1].cpu().numpy())
+        all_te_probas.append(te_probas)
+
